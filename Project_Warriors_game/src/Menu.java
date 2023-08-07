2db16615ea01d69580c9b28f0abe1c00a1ea481c
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-08-07 11:49:53 +0200

    La fonctionnalité de mise à jour des infos est fonctionnelle

--- Project_Warriors_game/src/Menu.java
+++ Project_Warriors_game/src/Menu.java
@@ -54,23 +54,24 @@ public class Menu {
         Scanner keyboard = new Scanner(System.in);
         String weapon;
         String shield;
+        String class_name = "Warrior";
 
         System.out.println("Veuillez renseigner les caractéristiques du Guerrier");
 
         System.out.println("Entrez le nom du Guerrier");
         this.name = keyboard.nextLine();
-        submenu_character_creation(name, health, force);
+        submenu_character_creation(name, health, force, class_name);
         do {
             System.out.println("Entrez la vie du Guerrier (entre 5 et 10)");
             health = keyboard.nextInt();
         } while (health < 5 || health > 10);
-        submenu_character_creation(name, health, force);
+        submenu_character_creation(name, health, force, class_name);
         do {
             System.out.println("Entrez la force du Guerrier (entre 5 et 10)");
             force = keyboard.nextInt();
             keyboard.nextLine(); // Doublement, car il considère entrée comme une chaine vide du coup weapon serait " "
         } while (force < 5 || force > 10);
-        submenu_character_creation(name, health, force);
+        submenu_character_creation(name, health, force, class_name);
         System.out.println("Entrez le nom de l'arme du Guerrier");
         weapon = keyboard.nextLine();
         System.out.println("Entrer le nom du bouclier du Guerrier");
@@ -83,36 +84,41 @@ public class Menu {
         return new_warrior;
     }
 
-    public void wizards_creation() {
+    public Wizards wizards_creation() {
         Scanner keyboard = new Scanner(System.in);
         String spell;
         String potion;
+        String class_name = "Wizard";
 
         System.out.println("Veuillez renseigner les caractéristiques du Mage");
-
         System.out.println("Entrez le nom du Mage");
         name = keyboard.nextLine();
+        submenu_character_creation(name, health, force, class_name);
         do {
             System.out.println("Entrez la vie du Mage (entre 3 et 6)");
             health = keyboard.nextInt();
         } while (health < 3 || health > 6);
+        submenu_character_creation(name, health, force, class_name);
         do {
             System.out.println("Entrez la force du Mage (entre 8 et 15)");
             force = keyboard.nextInt();
             keyboard.nextLine(); // Le doublement comme dessus
         } while (force < 8 || force > 15);
+        submenu_character_creation(name, health, force, class_name);
         System.out.println("Entrez le nom du sort du Mage");
         spell = keyboard.nextLine();
+        submenu_character_creation(name, health, force,class_name);
         System.out.println("Entrez le nom de la potion du Mage");
         potion = keyboard.nextLine();
+        submenu_character_creation(name, health, force, class_name);
 
         Wizards new_wizard = new Wizards(name, health, force, spell, potion);
 
         System.out.println("Voici les caractéristiques de votre nouveau Guerrier\n" + "Nom : " + new_wizard.name + "\n Santé "
                 + new_wizard.health + "\n Force : " + new_wizard.force + "\n Sort : " + new_wizard.spell + "\n Potion : " + new_wizard.potion);
-
+        return new_wizard;
     }
-    public void submenu_character_creation (String name, int health, int force ) {
+    public void submenu_character_creation (String name, int health, int force, String class_name) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Que voulez-vous faire ? \n Afficher les infos du personnage : 1 \n" +
                 "Modifier les infos du personnage : 2 \n Poursuivre la création : 3");
@@ -120,17 +126,22 @@ public class Menu {
         switch (keyboard.nextInt()) {
             case 1:
                 System.out.println("Voici les caractéristiques du personnages \n" + "Nom " + name + "\n" + "Santé" + health + "\n" + "Force" + force);
-                submenu_character_creation(name, health, force);
+                submenu_character_creation(name, health, force, class_name);
                 break;
             case 2:
-                update_character(name, health, force);
+                if (class_name.equals("Warrior")) {
+                    update_warriors(name, health, force);
+                }
+                else if (class_name.equals("Wizard")){
+                    update_wizards(name, health, force);
+                }
 
             case 3:
                 System.out.println("Reprise de la création du personnage");
                 break;
             }
         }
-    public void update_character (String name, int health, int force) {
+    public void update_warriors (String name, int health, int force) {
         Scanner keyboard = new Scanner(System.in);
         Warriors warrior = new Warriors();
         System.out.println("Modification du personnage");
@@ -138,11 +149,15 @@ public class Menu {
         System.out.println("Nouveau nom : ");
         name = keyboard.nextLine();
         System.out.println("ancienne santé : " + health);
-        System.out.println("Nouvelle santé : ");
-        health = keyboard.nextInt();
+        do {
+            System.out.println("Entrez la nouvelle santé du Guerrier (entre 5 et 10)");
+            health = keyboard.nextInt();
+        } while (health < 5 || health > 10);
         System.out.println("Ancienne force : " + force);
-        System.out.println("Nouvelle force : ");
-        force = keyboard.nextInt();
+        do {
+            System.out.println("Entrez la nouvelle force du Guerrier (entre 5 et 10)");
+            force = keyboard.nextInt();
+        } while (force < 5 || force > 10);
         keyboard.nextLine(); // Ignorer la ligne vide après avoir lu l'entier
 
         // Appeler la fonction update_warrior avec les nouvelles valeurs
@@ -152,6 +167,33 @@ public class Menu {
         System.out.println("Nom : " + warrior.getName());
         System.out.println("Santé : " + warrior.getHealth());
         System.out.println("Force : " + warrior.getForce());
+    }
+    public void update_wizards (String name, int health, int force) {
+        Scanner keyboard = new Scanner(System.in);
+        Wizards wizards = new Wizards();
+        System.out.println("Modification du personnage");
+        System.out.println("Ancien nom : " + name);
+        System.out.println("Nouveau nom : ");
+        name = keyboard.nextLine();
+        System.out.println("ancienne santé : " + health);
+        do {
+            System.out.println("Entrez la nouvelle santé du Mage (entre 5 et 10)");
+            health = keyboard.nextInt();
+        } while (health < 3 || health > 6);
+        System.out.println("Ancienne force : " + force);
+        do {
+            System.out.println("Entrez la nouvelle force du Mage (entre 5 et 10)");
+            force = keyboard.nextInt();
+        } while (force < 8 || force > 15);
+        keyboard.nextLine(); // Ignorer la ligne vide après avoir lu l'entier
+
+        // Appeler la fonction update_warrior avec les nouvelles valeurs
+        wizards.update_wizards(name, health, force);
+
+        System.out.println("Les informations du Guerrier ont été mises à jour :");
+        System.out.println("Nom : " + wizards.getName());
+        System.out.println("Santé : " + wizards.getHealth());
+        System.out.println("Force : " + wizards.getForce());
     }
 }
 
--- Project_Warriors_game/src/Wizards.java
+++ Project_Warriors_game/src/Wizards.java
@@ -12,4 +12,52 @@ public class Wizards {
         this.spell = spell;
         this.potion = potion;
     }
+    Wizards(){
+
+    }
+    public void update_wizards (String new_name, int new_health, int new_force) {
+        this.name = new_name;
+        this.health = new_health;
+        this.force = new_force;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    public String getSpell() {
+        return spell;
+    }
+
+    public void setSpell(String spell) {
+        this.spell = spell;
+    }
+
+    public String getPotion() {
+        return potion;
+    }
+
+    public void setPotion(String potion) {
+        this.potion = potion;
+    }
 }
